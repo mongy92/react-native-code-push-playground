27180318efc20fa4d9325c3f741ad86185c0ce6f
Ahmed Mongy <a.elzantot@gmail.com>
2021-04-06 22:56:41 +0200

    :bulb: install & integrate code push

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import codePush from 'react-native-code-push';
 
-AppRegistry.registerComponent(appName, () => App);
+const MyApp = codePush(App);
+
+AppRegistry.registerComponent(appName, () => MyApp);
