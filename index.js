a50805f4498c49840283403a39de6eda1698d798
Ahmed Mongy <a.elzantot@gmail.com>
2021-04-08 17:21:04 +0200

    customize code push for multi variants

--- index.js
+++ index.js
@@ -2,12 +2,27 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 import codePush from 'react-native-code-push';
 
-const MyApp = codePush(App);
+const codePushsKeys = Platform.select({
+  ios: {
+    STATGING: '9oU6z9xic4FiRtyI--ZZvpwlNGLkqmQhJGnEcV',
+    PRODUCTION: 'SH08OCFzA-mUGoFPBy0CScT9dbQof8z88R9rm',
+  },
+  android: {
+    STATGING: 'gesXwoHb_ktA06gROecBeSxGfmW7tM01vkj81',
+    PRODUCTION: 'rQDKzQWJLD1Vbh0vTJwCPdoYBQgSjLxWg7oFi',
+  },
+});
+
+const isBetaUser = false;
+
+const MyApp = codePush({
+  deploymentKey: isBetaUser ? codePushsKeys.STATGING : codePushsKeys.PRODUCTION,
+})(App);
 
 AppRegistry.registerComponent(appName, () => MyApp);
